b2c5a7a88942210755abc64dcecba4eabfa19c91
David Rottenfusser <David@Alanine.local>
2014-06-03 16:38:21 -0600

    Initial Commit

---
+++ Bouncing Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouncing Ball
+//
+//  Created by David Rottenfusser on 2014-06-03.
+//  Copyright (c) 2014 David Rottenfusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
